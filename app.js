54ef8747c04f32f4bc76c8e06f951da85ef0b7c2
João Paulo Dias Ventura <joaopdiasventura@gmail.com>
2023-12-05 23:21:09 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,35 @@
+import express from "express";
+import nodemailer from "nodemailer";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.post("/", (req, res) => {
+    const transporter = nodemailer.createTransport({
+        service: 'Gmail',
+        auth: {
+            user: req.body.from,
+            pass: req.body.password,
+        },
+    });
+
+    const mailOptions = {
+        from: req.body.from,
+        to: req.body.to,
+        subject: req.body.title,
+        text: req.body.content,
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.log('Erro ao enviar o e-mail: ' + error);
+        } else {
+            res.send("foi");
+        }
+    });
+
+});
+
+app.listen(PORT, () => console.log("SERVIÇO RODANDO NA PORTA " + PORT));
