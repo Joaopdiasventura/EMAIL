614459328f3c0e9c4533d6edf0467b97c284ecdc
João Paulo Dias <joaopdiasventura@gmail.com>
2024-07-08 18:21:45 -0300

    feat: enviando a autorização pelo corpo da requisição

--- app.js
+++ app.js
@@ -1,44 +1,40 @@
-import { config } from "dotenv";
-config();
-
 import express from "express";
 import cors from "cors";
-import nodemailer from "nodemailer";
+import { createTransport } from "nodemailer";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
 app.post("/", (req, res) => {
-    const transporter = nodemailer.createTransport({
-        service: 'Gmail',
-        auth: {
-            user: process.env.EMAIL,
-            pass: process.env.PASSWORD,
-        },
-    });
+  const transporter = createTransport({
+    service: "Gmail",
+    auth: {
+      user: req.body.email,
+      pass: req.body.password,
+    },
+  });
 
-    const mailOptions = {
-        from: process.env.EMAIL,
-        to: req.body.to,
-        subject: req.body.title,
-        text: `${req.body.content}`,
-    };
+  const mailOptions = {
+    from: req.body.email,
+    to: req.body.to,
+    subject: req.body.title,
+    text: `${req.body.content}`,
+  };
 
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            console.log('Erro ao enviar o e-mail: ' + error);
-            res.status(400).send(error)
-        } else {
-            res.send({
-                message: "Email Enviado com suscesso",
-                data: mailOptions.text
-            });
-        }
-    });
-
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log("Erro ao enviar o e-mail: " + error);
+      res.status(400).send(error);
+    } else {
+      res.send({
+        message: "Email Enviado com suscesso",
+        data: mailOptions,
+      });
+    }
+  });
 });
 
-app.listen(PORT, () => console.log("SERVIÇO RODANDO NA PORTA " + PORT));
+app.listen(PORT, () => console.log("SERVIDOR RODANDO NA PORTA " + PORT));
